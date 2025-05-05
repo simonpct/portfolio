bcbece869d8a4ca47084df507ca998bff2062506
Simon <94762919+simonpct@users.noreply.github.com>
2025-05-05 20:17:46 +0200

    fix: add links

--- src/app/components/SplashScreen/SplashScreen.tsx
+++ src/app/components/SplashScreen/SplashScreen.tsx
@@ -30,9 +30,12 @@ export default function SplashScreen() {
     <div className={clsx("fixed top-0 left-0 w-full h-full bg-[#E9F1FF] z-[1000]",
       isSplashScreenOpen ? "top-0" : "top-[-100%]",
       "transition-all duration-300 ease-in-out",
-      "flex items-center justify-center"
+      "flex items-center justify-center max-h-screen"
     )}>
       <Map />
+      <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 cursor-pointer" onClick={() => setIsSplashScreenOpen(false)}>
+        Ne plus voir
+      </span>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import Image from "next/image";
 import TimeLine from "./components/TimeLine/TimeLine";
 import WritingText from "./components/Text/WritingText";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
+import Link from "next/link";
+import { GithubLogo, LinkedinLogo, Mailbox } from "@phosphor-icons/react";
+import Particles from "./components/Overlays/Particles";
 
-export default function Home() {
+export default function Home(): React.ReactElement {
+
   return (
     <>
+      <Particles />
       <SplashScreen />
       
-      <div className="flex flex-col p-6">
-        <div className="flex gap-4 mb-12 p-1 sm:p-2 md:p-5">
+      <div className="flex flex-col p-6 z-10 justify-center">
+        <div className="flex gap-4 mb-6 p-1 sm:p-2 md:p-5 justify-center">
           <Image src="/image.png" alt="avatar" width={60} height={60} className="rounded-full w-16 h-16" />
           <div className="flex flex-col gap-0.75">
             <h1 className="text-3xl font-bold">
@@ -17,6 +24,12 @@ export default function Home() {
             </h1>
             <p>Développeur Web en alternance</p>
           </div>
+        </div>
+
+        <div className="flex gap-4 justify-center mb-15">
+          <Link href="https://linkedin.com/in/simon-picot"><LinkedinLogo size={24}/></Link>
+          <Link href="https://github.com/simonpct"><GithubLogo size={24}/></Link>
+          <Link href="mailto:picotsim@gmail.com"><Mailbox size={24}/></Link>
         </div>
 
         <TimeLine />
